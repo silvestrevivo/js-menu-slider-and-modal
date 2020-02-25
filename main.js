a312ea34af0af2110f64cfd916945e9d7df2899e
silvestrevivo <silvestrevivomillan@gmail.com>
2020-02-25 13:08:37 +0100

    javascript finished

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('Menu Slider And Modal');
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+// Toggle nav
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav');
+});
+
+// Show modal
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal');
+});
+
+// Hide modal
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal');
+});
+
+// Hide clicking outside modal
+window.addEventListener('click', e => {
+    if (e.target === modal) {
+        modal.classList.remove('show-modal');
+    }
+});
